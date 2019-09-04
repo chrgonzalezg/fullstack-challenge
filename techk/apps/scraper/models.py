ee3994df80d76d65fe8d105d25954fd60e9c6efa
Christhopher González <cgonzalez@careonlabs.com>
2019-09-04 13:22:20 -0400

    Creating Categories and Books models

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    c_id = models.IntegerField(primary_key=True)
+    c_name = models.CharField(max_length=45)
+
+class Books(models.Model):    
+    b_id = models.IntegerField(primary_key=True)    
+    b_title = models.CharField(max_length=80)
+    b_thumbnail = models.CharField(max_length=125)
+    b_price = models.CharField(max_length=20)
+    b_stock = models.BooleanField()
+    b_product_descripcion = models.TextField()
+    b_upc = models.CharField(max_length=30)
+    fk_c_id_category = models.ForeignKey(Categories, on_delete=models.CASCADE)
