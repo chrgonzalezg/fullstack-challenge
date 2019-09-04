957d3e4c56980f7f0e30eb885208ec31c7dd6667
Christhopher González <cgonzalez@careonlabs.com>
2019-09-04 15:20:06 -0400

    Create Serializers of models

---
+++ techk/apps/scraper/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Books, Categories
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = ("c_id", "c_name")
+
+class BooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Books
+        fields = ("b_id", "b_title", "b_thumbnail", "b_price", "b_stock", "b_product_descripcion", "b_upc", "fk_c_id_category")
