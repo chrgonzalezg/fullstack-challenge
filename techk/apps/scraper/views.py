4361f4566bdc0c95f1a2be9f344e7a4f406a07e4
Christhopher González <cgonzalez@careonlabs.com>
2019-09-09 10:38:31 -0300

    Implement multiprocessing and fetch books for the button

--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse, JsonResponse
 from django.utils.html import escape
 from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter, OrderingFilter
+from multiprocessing import Pool
 
 # Create your views here.
 
@@ -45,32 +46,44 @@ def fetchCategory(request):
 
 
 
-def fetchBooks(request):
-    
+def generateUrls():
     totalPages = int((re.findall('\d+', soup.find("li", { "class" : "current" }).text ))[1])
-
-    booksAdded = 0
+    all_urls = list()
     for i in range(1, totalPages+1):
-        soup_books = BeautifulSoup(requests.get(URL_TO_SCRAP+"catalogue/category/books_1/page-"+str(i)+".html").text, 'html.parser')
-        #soup_books = BeautifulSoup(requests.get(URL_TO_SCRAP+"catalogue/category/books_1/page-1.html").text, 'html.parser')        
-        booksOfPages = [i.text for i in soup_books.findAll('article', {'class': 'product_pod'})]        
-        for booksOfPages in soup_books.select("article div a"):
-            urlBook = URL_TO_SCRAP+"catalogue/"+booksOfPages['href'][6:]
-            soupBook = BeautifulSoup(requests.get(urlBook).text, 'html.parser')
-            desc = soupBook.find('p', {'class' : False})
-            data_dict = {                
-                "b_id": int(re.search(r'(\d+)\D+$', urlBook).group(1)),
-                "b_title": soupBook.find('h1').text,
-                "b_thumbnail": URL_TO_SCRAP+soupBook.find('img')['src'][6:],
-                "b_price": float(soupBook.find("p", { "class" : "price_color" }).text[2:]),
-                "b_stock": int(re.findall('\d+', soupBook.find('p',{'class':'instock availability'}).text)[0]),
-                "b_product_descripcion": desc.text if desc else "" ,
-                "b_upc": soupBook.find('td').text,
-                "categories": Categories.objects.get(c_id = re.findall('\d+', soupBook.find_all('a')[3]["href"])[0]),
-            }
-            try:
-                Books(**data_dict).save()
-                booksAdded += 1
-            except IntegrityError as e:
-                continue            
-    return JsonResponse({'status': str(booksAdded)+" books has added"}, status=200)
+        soup_books = URL_TO_SCRAP+"catalogue/category/books_1/page-"+str(i)+".html"
+        all_urls.append(soup_books)
+    return all_urls
+
+def scrape(url):
+    soup_books = BeautifulSoup(requests.get(url).text, 'html.parser')
+    #soup_books = BeautifulSoup(requests.get(URL_TO_SCRAP+"catalogue/category/books_1/page-1.html").text, 'html.parser')        
+    booksOfPages = [i.text for i in soup_books.findAll('article', {'class': 'product_pod'})]        
+    for booksOfPages in soup_books.select("article div a"):
+        urlBook = URL_TO_SCRAP+"catalogue/"+booksOfPages['href'][6:]
+        print(urlBook)
+        soupBook = BeautifulSoup(requests.get(urlBook).text, 'html.parser')
+        desc = soupBook.find('p', {'class' : False})
+        data_dict = {                
+            "b_id": int(re.search(r'(\d+)\D+$', urlBook).group(1)),
+            "b_title": soupBook.find('h1').text,
+            "b_thumbnail": URL_TO_SCRAP+soupBook.find('img')['src'][6:],
+            "b_price": float(soupBook.find("p", { "class" : "price_color" }).text[2:]),
+            "b_stock": int(re.findall('\d+', soupBook.find('p',{'class':'instock availability'}).text)[0]),
+            "b_product_descripcion": desc.text if desc else "" ,
+            "b_upc": soupBook.find('td').text,
+            "categories": Categories.objects.get(c_id = re.findall('\d+', soupBook.find_all('a')[3]["href"])[0]),
+        }
+        try:
+            Books(**data_dict).save()
+        except IntegrityError as e:
+            continue
+
+def fetchBooks(request):
+    try:
+        p = Pool(50)
+        p.map(scrape, generateUrls())
+        p.terminate()
+        p.join()
+    except requests.exceptions.ConnectionError as e:
+        return JsonResponse({'status': 'Connection error, try again to fetch'}, status=200)                   
+    return JsonResponse({'status': 'The books have been added correctly'}, status=200)
